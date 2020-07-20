1f636762cda427090a1131e0b69755a4eeeaddbe
vishal199297 <vishal199297@gmail.com>
2020-07-20 10:31:00 +0530

    Gradle and Docker setup

---
+++ src/test/java/com/abbf/demo/VithiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abbf.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VithiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
